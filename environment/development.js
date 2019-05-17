7432a01b3e397b0bbc05e778b50ee62c816a0183
hades <quentindevial19@gmail.com>
2019-05-17 20:10:52 +0200

    first commit

---
+++ environment/development.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+  dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
+  cert: path.join( __dirname, '../ssl/local.crt'),
+  key: path.join( __dirname, '../ssl/local.key'),
+
+}
---
+++ environment/production.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+  dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
+  cert: path.join( __dirname, ''),
+  key: path.join( __dirname, ''),
+}
