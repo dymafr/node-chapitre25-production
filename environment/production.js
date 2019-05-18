92de00180cee4f626560d2638f76cf35070a3642
hades <quentindevial19@gmail.com>
2019-05-18 15:49:09 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,6 @@
-const path = require('path');
 
 module.exports = {
   dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
-  cert: path.join( __dirname, '/etc/letsencrypt/live/www.dyma-projects.site/fullchain.pem'),
-  key: path.join( __dirname, '/etc/letsencrypt/live/www.dyma-projects.site/privkey.pem'),
+  cert: '/etc/letsencrypt/live/www.dyma-projects.site/fullchain.pem',
+  key: '/etc/letsencrypt/live/www.dyma-projects.site/privkey.pem',
 }
