a9580914262ddf031f01d07405840b3bdb819bd9
hades <quentindevial19@gmail.com>
2019-05-18 15:47:09 +0200

    second commit

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
   dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
-  cert: path.join( __dirname, ''),
-  key: path.join( __dirname, ''),
+  cert: path.join( __dirname, '/etc/letsencrypt/live/www.dyma-projects.site/fullchain.pem'),
+  key: path.join( __dirname, '/etc/letsencrypt/live/www.dyma-projects.site/privkey.pem'),
 }
